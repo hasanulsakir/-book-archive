2a793e7345cc8615bfd1550dfbd1eac73b865ef7
hasanulsakir <khantanvir03@example.com>
2021-09-02 15:17:13 +0600

    code push

---
+++ JS/app.js
@@ -0,0 +1,110 @@
+const searchBook = () => {
+    const searchValue = document.getElementById('inputBook');
+    const searchText = searchValue.value;
+
+
+    searchValue.value = '';
+    notification('hidden')
+    resultCount('none')
+    searchResult('hidden')
+    toggleSpinner('block')
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBook(data.docs.slice(0, 20)))
+    fetch(url)
+        .then(res => res.json())
+        .then(data => numFound(data.numFound))
+
+}
+
+// spinner show on load 
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+
+
+}
+
+// spinner loading Disable result 
+
+const searchResult = displayStyle => {
+    document.getElementById('search-result').style.visibility = displayStyle;
+
+
+}
+
+// Disable Enable message 
+const notification = displayStyle => {
+    document.getElementById('notify').style.visibility = displayStyle;
+
+
+
+}
+const resultCount = displayStyle => {
+    document.getElementById('totalBook').style.display = displayStyle;
+    document.getElementById('result').style.display = displayStyle;
+
+
+}
+
+// total boook count
+const numFound = numFound => {
+    if (numFound !== 0) {
+        resultCount('inline-block')
+        document.getElementById('totalBook').innerText = `${numFound} Book  `;
+
+
+    }
+
+}
+
+
+
+
+
+// show result 
+
+const displayBook = books => {
+    const searchBook = document.getElementById('search-result');
+    searchBook.innerHTML = '';
+    console.log(books);
+    if (books.length === 0) {
+
+        document.getElementById('notify').innerText = "Book Not Found !";
+        notification('visible');
+        resultCount('none');
+        toggleSpinner('none');
+    } else {
+        document.getElementById('result').innerText = `Show ${books.length} Of `;
+        resultCount('inline-block');
+
+        document.getElementById('notify').innerText = "";
+        books.forEach(book => {
+
+            const div = document.createElement('div');
+
+            div.classList.add('col');
+            div.innerHTML = `
+                        <div class="card shadow-sm">
+                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-thumbnail img-fluid" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${book.title.slice(0, 15)}</h5>
+                            <p class="card-text"><span class="text-secondary">Author:</span> ${book?.author_name}</p>
+                            <p class="card-text"><span class="text-secondary">Publisher:</span> ${book?.publisher}</p>
+                            <p class="card-text"><span class="text-secondary">1st Published:</span> ${book.first_publish_year}</p>
+                        </div>
+                        `
+            searchBook.appendChild(div);
+
+
+        });
+        searchResult('visible')
+        toggleSpinner('none')
+
+
+
+
+    }
+}
